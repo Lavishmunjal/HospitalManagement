02321744a8db94e7266d18be024ec9838fd1756d
Lavishmunjal <0111lavishmunjal@gmail.com>
2024-10-23 21:44:26 +0530

    Doctors details added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,6 +3,7 @@ import ErrorHandler from "../middlewares/errorMiddleware.js"
 import { User } from "../models/userSchema.js";
 // import {comparePassword} from "..models/userSchema.js"
 import {generateToken} from "../utils/jwtToken.js"
+import cloudinary from "cloudinary"
 
 export const patientregister = catchAsyncError(async(req, res, next)=>{
 const {firstName, lastName, email, gender, phone, password, dob, nic, role}=req.body;
@@ -27,11 +28,15 @@ export const login = catchAsyncError(async(req, res, next)=>{
     if(password!==confirmPassword) {
         return next (new ErrorHandler("Password and confirmpassword do not match", 400))
     }
+    
     const user = await User.findOne({email}).select("+password")
+    
 
     if(!user) {
         return next (new ErrorHandler("Invalid pw or email", 400))
     }
+    
+    
 
     const isPasswordMatched = await user.comparePassword(password)
     if(!isPasswordMatched) {
@@ -72,4 +77,110 @@ export const getAllDoctors = catchAsyncError(async(req, res, next)=>{
         success:true,
         doctors
     })
+})
+export const getUserDetails = catchAsyncError(async(req, res, next)=>{
+    const user = req.user;
+    res.status(200)
+    .json({
+        success:true,
+        user, 
+    })
+})
+
+export const logoutAdmin = catchAsyncError(async(req, res, net)=>{
+    res.status(200).cookie("adminToken", "",{
+        httpOnly:true,
+        expires: new Date(Date.now())
+    })
+    .json({
+        success:true,
+        message:"User logged out successfully"
+    })
+})
+
+export const logoutPatient = catchAsyncError(async(req, res, net)=>{
+    res.status(200).cookie("patientToken", "",{
+        httpOnly:true,
+        expires: new Date(Date.now())
+    })
+    .json({
+        success:true,
+        message:"User logged out successfully"
+    })
+})
+
+export const addNewDoctor = catchAsyncError(async(req, res, next)=>{
+    if(!req.files || Object.keys(req.files).length==0) {
+        return next (new ErrorHandler("Doctor avatar required", 400))
+    }
+    const {docAvatar} = req.files;
+    const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
+    if (!allowedFormats.includes(docAvatar.mimetype)) {
+        return next(new ErrorHandler("File Format Not Supported!", 400));
+    }
+    const {
+        firstName,
+        lastName,
+        email,
+        phone,
+        nic,
+        dob,
+        gender,
+        password,
+        doctorDepartment,
+      } = req.body;
+      if (
+        !firstName ||
+        !lastName ||
+        !email ||
+        !phone ||
+        !nic ||
+        !dob ||
+        !gender ||
+        !password ||
+        !doctorDepartment ||
+        !docAvatar
+      ) {
+        return next(new ErrorHandler("Please Fill Full Form!", 400));
+      }
+      const isRegistered = await User.findOne({ email });
+     if (isRegistered) {
+        return next(
+        new ErrorHandler(`${isRegistered.role} With This Email Already Exists!`, 400)
+     );
+     }
+
+     const cloudinaryResponse = await cloudinary.uploader.upload(
+        docAvatar.tempFilePath
+      );
+      if (!cloudinaryResponse || cloudinaryResponse.error) {
+        console.error(
+          "Cloudinary Error:",
+          cloudinaryResponse.error || "Unknown Cloudinary error"
+        );
+        return next(
+          new ErrorHandler("Failed To Upload Doctor Avatar To Cloudinary", 500)
+        );
+      }
+      const doctor = await User.create({
+        firstName,
+        lastName,
+        email,
+        phone,
+        nic,
+        dob,
+        gender,
+        password,
+        role: "Doctor",
+        doctorDepartment,
+        docAvatar: {
+          public_id: cloudinaryResponse.public_id,
+          url: cloudinaryResponse.secure_url,
+        },
+      })
+      res.status(200).json({
+        success:true,
+        message:"new Doctor registered",
+        doctor
+      })    
 })
